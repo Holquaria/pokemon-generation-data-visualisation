cb9626cce6697d9eb1bde99fc1de1addb35b2426
Holquaria <jholker4@gmail.com>
2023-10-18 22:26:08 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,197 @@
-import Image from 'next/image'
+"use client"
+
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useEffect, useRef, useState } from 'react'
+import pokemonData from './data/pokemon-types-by-generation.json'
+import { OrbitControls, PerspectiveCamera, Text, Text3D } from '@react-three/drei'
+import { DoubleSide } from 'three'
+
+type GenerationData = {
+  generation: {
+    types: {
+
+    },
+    pokemon: [string]
+  }
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const meshRef = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  const [data, setData] = useState([] as any)
+  const [generation, setGeneration] = useState(0)
+  const [typeAndGeneration, setTypeAndGeneration] = useState([])
+
+  const colors = {
+    grass: '#52b146',
+    poison: '#9454cb',
+    psychic: '#e96a8d',
+    steel: '#6caec9',
+    rock: '#8c886c',
+    normal: '#dcd8df',
+    fairy: '#e08dde',
+    ice: '#90efef',
+    dark: '#4f4747',
+    ground: '#b7844d',
+    dragon: '#596fbf',
+    water: '#59b3ee',
+    ghost: '#6f466f',
+    electric: '#f1d461',
+    flying: '#a8d5f6',
+    bug: '#cacb5a',
+    fighting: '#e59222',
+    fire: '#e6623f'
+  }
+
+  const generations = [
+    'Generation 1: Red, Blue, Yellow',
+    'Generation 2: Silver, Gold, Crystal',
+    'Generation 3: Ruby, Sapphire, Emerald',
+    'Generation 4: Diamond, Pearl, Platinum',
+    'Generation 5: Black and White',
+    'Generation 6: X and Y',
+    'Generation 7: Sun and Moon',
+    'Generation 8: Sword and Shield',
+    'Generation 9: Scarlet and Violet'
+  ]
+
+  console.log('typeAndGeneration', typeAndGeneration)
+
+  const yAxis = [0, 5, 10, 15, 20, 25, 30, 35]
+
+  useEffect(() => {
+    setData(pokemonData)
+  }, [])
+
+  const Box = (props: any) => {
+
+    // useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    return (
+      <mesh
+        {...props}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+  }
+
+  const Bar = ({props}) => {
+    const [active, setActive] = useState(false)
+    return <mesh
+    key={props.key}
+    // scale={active ? 1.2 : 1}
+    position={[(props.key * 2), (((props.type1 / 2))), active ? 1 : 0]}
+    onClick={(event) => setTypeAndGeneration(data[generation][generation].pokemon[props.type0])}
+    onPointerOver={event => setActive(!active)}
+    onPointerOut={(event) => setActive(!active)}
+  >
+    <boxGeometry args={[1, props.type1, 1]} />
+    <meshPhysicalMaterial color={colors[props.type0]} />
+  </mesh>
+  }
+
+  const BarsByGeneration = () => {
+    if (data) {
+      return (
+        <mesh key={''} position={[-15, -10, 0]}>
+          <mesh
+            position={[-2, 17, 0]}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <planeGeometry args={[0.5, 35]} />
+            <meshPhysicalMaterial color={'grey'} side={DoubleSide} />
+          </mesh>
+          <mesh>
+            {yAxis.map((number, i) => {
+              return <Text3D
+                key={number}
+                font="./fonts/helvetiker_regular.typeface.json"
+                position={[-5, ((i * 5)), 0]}
+                rotation={[0, 0, 0]}
+              >
+                {number}
+                <meshPhysicalMaterial color='white'/>
+              </Text3D>
+            })}
+          </mesh>
+          <mesh
+            position={[16.75, -0.25, 0]}
+          >
+            <planeGeometry args={[38, 0.5]} />
+            <meshPhysicalMaterial color={'grey'} side={DoubleSide} />
+          </mesh>
+          {(Object.entries(data[generation][generation].types)).map((type, j) => {
+            return <mesh key={''}>
+              <Text3D
+                font="./fonts/helvetiker_regular.typeface.json"
+                position={[(j * 2), -0.5, 0]}
+                rotation={[0, 0, -1.5]}
+              >
+                {/* <meshStandardMaterial /> */}
+                {type[0]}
+                <meshPhysicalMaterial color='white'/>
+              </Text3D>
+              <Bar props={{key: j, type0: type[0], type1: type[1]}} />
+
+            </mesh>
+          })}
+        </mesh>
+      )
+    }
+  }
+
+  const GenerationSelector = () => {
+    return (
+      <div className='flex flex-row gap-4 justify-center my-2 absolute left-1/2 -translate-x-1/2 z-20'>
+        <h2>Select A Generation:</h2>
+        {generations.map((gen, i) => {
+          return <button key={gen} onClick={() => setGeneration(i)}>{(i + 1)}</button>
+        })}
       </div>
+    )
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+  const GenerationHeader = () => {
+    return <h1 className='justify-center flex absolute top-10 left-1/2 -translate-x-1/2 z-20'>{generations[generation]}</h1>
+  }
+
+  const PokemonList = () => {
+    if (typeAndGeneration.length !== 0) {
+      return (
+      <div className='flex flex-row gap-2 flex-wrap justify-center flex absolute bottom-2 left-1/2 -translate-x-1/2 z-20'>
+        {typeAndGeneration.map((pokemon) => {
+          console.log(pokemon)
+          return <p key={pokemon}>{pokemon}</p>
+        })}
       </div>
+      )
+    } else {
+      return <div className='flex flex-row gap-2 flex-wrap justify-center flex absolute bottom-2 left-1/2 -translate-x-1/2 z-20'>
+        <p>Select a type to see the Pokemon added in this generation</p>
+    </div>
+    }
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  return (
+    <main className="w-full h-full">
+      <GenerationSelector />
+      <GenerationHeader />
+      <div className='z-10 w-full h-full'>
+        <Canvas>
+          <PerspectiveCamera makeDefault fov={75} near={0.1} far={1000} position={[15, 15, 40]} />
+          <ambientLight intensity={0.2} />
+          <directionalLight position={[15, 15, 40]} castShadow={true} />
+          {/* <Bars /> */}
+          <BarsByGeneration />
+          <OrbitControls />
+        </Canvas>
       </div>
+      <PokemonList />
+      {/* <Data /> */}
     </main>
   )
 }
